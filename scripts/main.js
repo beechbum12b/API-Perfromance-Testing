91c319aec5953365f6d5a6254ab2fd5c8ac64129
Andrew Cox <ACox@NUV-ACox-Mac.local>
2025-08-16 11:16:53 -0500

    Code cleanup

--- scripts/main.js
+++ scripts/main.js
@@ -2,12 +2,16 @@ import { check, sleep } from "k6";
 import http from "k6/http";
 
 export const options = {
-  vus: 100,
-  duration: "1m"
+  vus: 30,
+  duration: "30s"
 };
 
 export default function() {
-  const response = http.get("https://dev-api.acexr.com/platform-services/v2/users/me");
+  const response = http.get("https://dev-api.acexr.com/platform-services/v2/health");
   check(response, { "status is 200": (r) => r.status === 200 });
   sleep(1);
+  const response2= http.get("https://dev-api.acexr.com/platform-services/v2/client-configs");
+  check(response, { "status is 200": (r) => r.status === 200 });
+  sleep(1)
+
 }
